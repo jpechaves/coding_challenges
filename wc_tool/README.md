ef9737003d8c8ad95191c6ab25c7be726bb0897d
João Pedro <pedrochaveslimas3@gmail.com>
2025-02-21 16:05:27 -0300

    Add final step to wc challenge and refactor it

--- wc_tool/README.md
+++ wc_tool/README.md
@@ -10,3 +10,36 @@ The Unix command line tools are a great metaphor for good software engineering a
 Click the original link for detailed steps and additional references!
 
 > [challenge-wc](https://codingchallenges.fyi/challenges/challenge-wc)
+
+## Steps
+
+- Step 1 ✅
+- Step 2 ✅
+- Step 3 ✅
+- Step 4 ✅
+- Step 5 ✅
+- Step final ✅
+
+## Usage
+
+```ruby
+$ ./wc.rb --help
+Usage: wc.rb [options]
+    -c, --bytes                      print the byte counts
+    -l, --lines                      print the newline counts
+    -w, --words                      print the word counts
+    -m, --chars                      print the character counts
+
+$ ./wc.rb test.txt
+"7145 58164 342190 test.txt"
+
+$ cat test.txt | ./wc.rb
+"7145 58164 342190"
+```
+
+## Ruby
+
+```sh
+$ ruby --version
+ruby 3.4.2 (2025-02-15 revision d2930f8e7a) +PRISM [x86_64-linux]
+```
--- wc_tool/wc.rb
+++ wc_tool/wc.rb
@@ -18,35 +18,46 @@ def set_defaults!
   OPTIONS[:bytes] = OPTIONS[:lines] = OPTIONS[:words] = true if OPTIONS.empty?
 end
 
+def set_data_stream!
+  @filename = nil
+  @data_stream = ""
+
+  if ARGV.empty? && !STDIN.tty?
+    @data_stream = STDIN.read
+  else
+    @filename = ARGV.shift
+    @data_stream = File.read(@filename)
+  end
+end
+
 def append_output(key, output)
   OUTPUT << output if OPTIONS[key]
 end
 
 def byte_counts
-  File.read(FILENAME, mode: "rb").bytesize
+  @data_stream.bytesize
 end
 
 def newline_counts
-  File.readlines(FILENAME).size
+  @data_stream.lines.size
 end
 
 def words_count
-  File.read(FILENAME).split(" ").size
+  @data_stream.split(" ").size
 end
 
 def chars_count
-  File.read(FILENAME).chars.size
+  @data_stream.chars.size
 end
 
 initialize_options!
 set_defaults!
-
-FILENAME = ARGV.shift
+set_data_stream!
 
 append_output(:lines, newline_counts)
 append_output(:words, words_count)
 append_output(:bytes, byte_counts)
 append_output(:chars, chars_count)
 
-OUTPUT << FILENAME
-p OUTPUT.join(" ")
+OUTPUT << @filename
+p OUTPUT.compact.join(" ")
